deb5f2b55ba4d783e0be1913052b94f208a8b3d7
Byron Rolle JR <dareal4our@gmail.com>
2025-03-07 23:05:21 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+let cardNumber = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+let cardSuite = ["♥","♦","♣","♠"]
+
+
+let randomNumber = Math.floor(Math.random() * cardNumber.length);
+let randomSuite = Math.floor(Math.random() * cardSuite.length);
+
+const topSuite = document.querySelector(".topSuite")
+const midNum = document.querySelector(".midNum")
+const botSuite = document.querySelector(".botSuite")
+
+if (cardSuite[randomSuite] == "♥" || cardSuite[randomSuite] == "♦" ) { 
+      topSuite.style.color = "red"
+      botSuite.style.color ="red"
+      midNum.style.color = "red"
+}
+
+topSuite.innerHTML = cardSuite[randomSuite];
+midNum.innerHTML = cardNumber[randomNumber];
+botSuite.innerHTML = cardSuite[randomSuite];
+
+// document.getElementById('.card').classList.add(getSuiteClass(finalSuit));
+
   console.log("Hello Rigo from the console!");
 };
+
